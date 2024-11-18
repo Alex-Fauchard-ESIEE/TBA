f7d412c4e29d43392f419831081967f2ab303247
Alex Fauchard <alex.fauchard@edu.esiee.fr>
2024-11-18 18:00:36 +0000

    doctstring pour Room

--- room.py
+++ room.py
@@ -1,7 +1,49 @@
 # Define the Room class.
 
 class Room:
+    """
+    La classe Room donne des méthodes pour la pièce dans lequel le joueur et une description de celle-ci.
 
+    une description plus détaillée de la classe (optionnel) ;
+
+    Les attributs de la classe sont :
+        name : nom de la pièce
+        description : une description de la pièce
+        exits : les différentes sorties de la pièce
+
+    Les méthodes de la classe sont :
+        __init__() : constructeur
+        get_exit() : donne les sorties possibles en fonction de la direction donnée
+        get_exit_string() : donne une description de la pièce vers laquelle amène la sortie
+        get_long_description() : donne une description précise de la pièce actuelle ainsi que les sorties
+
+    Les exceptions levées par la classe sont :
+        None
+
+    Doctests :
+    >>> cave = Room("Cave", "dans une grotte profonde et sombre. Des voix semblent provenir des profondeurs.")
+    >>> cottage = Room("Cottage", "dans un petit chalet pittoresque avec un toit de chaume. Une épaisse fumée verte sort de la cheminée.")
+    >>> swamp = Room("Swamp", "dans un marécage sombre et ténébreux. L'eau bouillonne, les abords sont vaseux.")
+    >>> castle = Room("Castle", "dans un énorme château fort avec des douves et un pont levis. Sur les tours, des flèches en or massif.")
+    >>> forest = Room("Forest", "dans une forêt enchantée. Vous entendez une brise légère à travers la cime des arbres.")
+    >>> tower = Room("Tower", "dans une immense tour en pierre qui s'élève au dessus des nuages.")
+    >>> forest.exits = {"N" : cave, "E" : tower, "S" : castle, "O" : None}
+    >>> tower.exits = {"N" : cottage, "E" : None, "S" : swamp, "O" : forest}
+    >>> forest.name
+    'Forest'
+    >>> tower.name
+    'Tower'
+    >>> forest.description
+    'dans une forêt enchantée. Vous entendez une brise légère à travers la cime des arbres.'
+    >>> tower.description
+    "dans une immense tour en pierre qui s'élève au dessus des nuages."
+    >>> forest.get_exit("N") == cave
+    True
+    >>> tower.get_exit("E")
+    
+    >>> forest.get_exit_string()
+    'Sorties: N, E, S'
+    """
     # Define the constructor. 
     def __init__(self, name, description):
         self.name = name
