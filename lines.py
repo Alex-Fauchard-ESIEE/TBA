e95f9557a05675d52486e88561d965881e5ed138
Alex Fauchard <alex.fauchard@edu.esiee.fr>
2025-01-08 16:06:00 +0000

    maj dialogues

--- lines.py
+++ lines.py
@@ -10,16 +10,16 @@ Mais voilà qu'après plusieurs jours à galoper sans arrêt, vous décidez de v
 Clyde : Ce petit hôtel fera très bien l'affaire pour me reposer"""], 
 'RECEPTIONNISTE' : [[lambda : input("\nRéceptionniste : Qu’est ce qui est plus grand que la Tour Eiffel, mais infiniment moins lourd ?\n"), "LA TOUR EIFFEL", "\nBravo !"]],
 'SHERIF' : [[lambda : input("""Shérif : Oui, mon cher, je l’ai vu dans la cellule d’un des prisonniers, il passe son temps à le contempler et à lui parler. Shérif : Bien sûr, mais avant il faudra répondre à l’énigme suivante: Qu'est-ce qui n'est pas vivant mais qui grandit, n'a pas de poumon mais a besoin d'air, et meurt sous l'eau ?\n """), "Clyde : Le feu",("""Shérif : Exact, maintenant tu peux aller voir le prisonnier mais fais attention il mord.""")]]
-'EXTRATERRESTRE' : [[lambda : input("""Extraterrestre : Moi savoir où trouver collier. Collier se trouvait en haut de la colline là bas dans temple, vous avoir besoin de 5h de marche. Mais attention là-bas y avoir gardien du temple dangereux.""")]]
-'GARDIEN DU TEMPLE' : [[lambda : input("""Gardien du temple : Moi savoir où trouver collier. Collier se trouvait en haut de la colline là bas dans temple, vous avoir besoin de 5h de marche. Mais attention là-bas y avoir gardien du temple dangereux. Gardien du temple : “Qu'est-ce qui t'appartient mais que les autres utilisent plus que toi?\n"""), "Clyde : Mon prénom"]]
-'BAKOU' : [[lamda : input("""Bakou : Merci pour ton aide précieuse, en quoi puis-je te venir en aide ? \nBakou : Je ne l’ai jamais vu ici, mais par contre je pense que tu peux la trouver sur une des deux lunes qui gravitent autour de ma planète. Néanmoins, une des deux lunes ne te laissera jamais revenir et tu mourras. Pour avoir le luxe d’être guidé il faut que tu répondes correctement à l’énigme suivante : 
+,'EXTRATERRESTRE' : [[lambda : input("""Extraterrestre : Moi savoir où trouver collier. Collier se trouvait en haut de la colline là bas dans temple, vous avoir besoin de 5h de marche. Mais attention là-bas y avoir gardien du temple dangereux.""")]]
+,'GARDIEN DU TEMPLE' : [[lambda : input("""Gardien du temple : Moi savoir où trouver collier. Collier se trouvait en haut de la colline là bas dans temple, vous avoir besoin de 5h de marche. Mais attention là-bas y avoir gardien du temple dangereux. Gardien du temple : “Qu'est-ce qui t'appartient mais que les autres utilisent plus que toi?\n"""), "Clyde : Mon prénom"]]
+,'BAKOU' : ["\nBakou : Merci pour ton aide précieuse, en quoi puis-je te venir en aide ?", [lambda : input("""\nBakou : Je ne l’ai jamais vu ici, mais par contre je pense que tu peux la trouver sur une des deux lunes qui gravitent autour de ma planète. Néanmoins, une des deux lunes ne te laissera jamais revenir et tu mourras. Pour avoir le luxe d’être guidé il faut que tu répondes correctement à l’énigme suivante : 
 
 Mon premier est une lettre de l’alphabet.
 Mon deuxième est un oiseau qui aime tout ce qui brille.
 Mon troisième est un talent particulier que l’on peut avoir.
 Mon tout a beaucoup de travail le jour de la Saint-Valentin.""")
-,"Cupidon"
-(\nBakou : C’est la bonne réponse tu vas pouvoir être guidé vers la bonne lune et ainsi pouvoir trouver ton collier.""")]]
+,"Cupidon",
+"""\nBakou : C’est la bonne réponse tu vas pouvoir être guidé vers la bonne lune et ainsi pouvoir trouver ton collier."""]]
 'JAR JAR BINKS' :[[lambda : input("""Jar Jar Binks : Bonsoir jeune voyageur, que viens tu faire dans ma ville ? \nJar Jar Binks : Tiens donc… tout dépend de l’individu mais l’avenir d’un homme peut se trouver soit dans une femme, soit dans le travail mais surement pas dans les jeux d’argent.
 Alors quel genre d’avenir êtes-vous venu chercher ? \nJar Jar Binks : Hmm je vois, tu es Clyde le célèbre tueur à gage. Je peux t’aider à obtenir ce que tu cherches pour te consoler. Mais il faudra répondre à l’énigme suivante : 
 
